c9f6cc286bf3e5afc248e2ebd3de7a895e06077f
OldCC <mjduerst@gmail.com>
2016-02-26 11:07:12 -0800

    Update init.py

--- init.py
+++ init.py
@@ -10,11 +10,11 @@ os.system("python /home/pi/kts/scripts/gps.py &")
 os.system("python /home/pi/kts/scripts/dst.py &")
 
 # wait 5 seconds to make sure they're inputing data
-time.sleep(10)
+time.sleep(5)
 os.system("python /home/pi/kts/scripts/gonkulator.py &")
 
 # wait a little longer to make sure all sentences are correct
-time.sleep(10)
+time.sleep(5)
 os.system("python /home/pi/kts/scripts/go.py &")
 
 # close 'er down
